86f0f7969b7b3003ce35ea313f1b14fa73c0d391
Michael-Brendo <59937684+Michael-Brendo@users.noreply.github.com>
2020-11-12 20:16:55 -0300

    Create Chao.cs

---
+++ Assets/Scripts/Chao.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chao : MonoBehaviour
+{
+    public float diferancaX;
+
+    public float minX;
+
+    private void Update()
+    {
+        if (transform.position.x <= minX)
+        {
+            transform.position = new Vector3(
+                transform.position.x + diferancaX * 2,
+                transform.position.y,
+                transform.position.z
+            );
+
+        }
+
+    }
+}
