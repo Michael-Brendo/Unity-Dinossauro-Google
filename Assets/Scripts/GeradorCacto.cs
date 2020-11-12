862078aec8fc277026a35eed88470798d57c4352
Michael-Brendo <59937684+Michael-Brendo@users.noreply.github.com>
2020-11-12 19:12:52 -0300

    Test

---
+++ Assets/Scripts/GeradorCacto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GeradorCacto : MonoBehaviour
+
+{
+    public GameObject[] cactoPrefabs;
+    public float delayInicial;
+    public float delayEntreCactos;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        InvokeRepeating("GerarCacto", delayInicial, delayEntreCactos);
+    }
+    // cria cactos aleatorio
+    private void GerarCacto()
+    {
+        var quantidadeCactos = cactoPrefabs.Length;
+        var indiceAleatorio = Random.Range(0, quantidadeCactos);
+        var cactoPrefab = cactoPrefabs[indiceAleatorio];
+        Instantiate(cactoPrefab, transform.position, Quaternion.identity);
+    }
+}
